e97cc07bc42af9c1d4361bf37087247678efdb92
AOI <hatake390o70@yahoo.co.jp>
2018-05-21 16:44:05 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,20 +3,30 @@
 @section('content')
 
    <h1>新規登録ページ</h1>
-  
-   
-   
-      {!! Form::model($task,['route' => 'tasks.store']) !!}
+
+<div class="row">
+
+<div class="col-sm-offset-2 col-sm-8">
+<div class="col-lg-offset-3 col-lg-6">
+
+ {!! Form::model($task, ['route' => 'tasks.store']) !!}
       
-         {!! Form::label('status','ステータス:') !!}
-         {!! Form::text('status') !!}
+       <div class="form-group">
+         {!! Form::label('status', 'ステータス:') !!}
+         {!! Form::text('status', null, ['class' => 'form-control']) !!}
+       </div>
+       
+       <div class="form-group">
+         {!! Form::label('content', 'タスク:') !!}
+         {!! Form::text('content', null, ['class' => 'form-control']) !!}
+       </div>
          
-         {!! Form::label('content','タスク:') !!}
-         
-         {!! Form::text('content') !!}
-         
-         {!! Form::submit('登録') !!}
+         {!! Form::submit('登録',['class'=>'btn btn-success']) !!}
       
       {!! Form::close() !!}
-
+</div>
+</div>
+</div>
+  
+     
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,21 +2,30 @@
 
 @section('content')
 
+<div class="row">
+   <div class="col-sm-offset-2 col-sm-8">
+   <div class="col-lg-offset-3 col-lg-6">
 
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-    
+        <div class="form-group">
         {!!Form::label('status','ステータス:') !!}
-        {!!Form::text('status') !!}
+        {!!Form::text('status',null,['class'=>'form-control']) !!}
+        </div>
         
+        <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::text('content',null,['class'=>'form-control']) !!}
+        </div>
+ 
+        {!! Form::submit('更新',['class'=>'btn btn-success']) !!}
 
     {!! Form::close() !!}
+   </div>
+   </div>
+</div>
 @endsection
 
 
